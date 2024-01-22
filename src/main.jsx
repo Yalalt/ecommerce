9f96151d799a19768a5d7d782ecbc394d59a12b1
Yalalt Gandush <yalaltgandush@gmail.com>
2024-01-22 23:10:32 +0800

    add cart Layout

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Auth0Provider } from '@auth0/auth0-react';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Index from './pages/index.jsx';
 import About from './pages/about.jsx';
 import Product from './pages/product.jsx';
 import Layout from './components/Layout.jsx';
+import ShoppingCard from './pages/shopping-card.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-    <Layout>
-      <Routes>
-        <Route path='/' element={<Index />} />
-        <Route path='/product/:id' element={<Product />} />
-        <Route path='/about' element={<About />} />
-        <Route path='*' element={<h2>404 - Not found page</h2>} />
-      </Routes>
-    </Layout>
-    </BrowserRouter>
+    <Auth0Provider
+      domain="dev-nqx4on6mv028ejjo.us.auth0.com"
+      clientId="IgxlRSAwaLkDG448Wl3I9z3NTQaYozvC"
+      authorizationParams={{
+        redirect_uri: window.location.origin
+      }}
+    >
+      <BrowserRouter>
+        <Layout>
+          <Routes>
+            <Route path='/' element={<Index />} />
+            <Route path='/product/:id' element={<Product />} />
+            <Route path='/shopping-card' element={<ShoppingCard />} />
+            <Route path='/about' element={<About />} />
+            <Route path='*' element={<h2>404 - Not found page</h2>} />
+          </Routes>
+        </Layout>
+      </BrowserRouter>
+    </Auth0Provider>
   </React.StrictMode>,
 )
